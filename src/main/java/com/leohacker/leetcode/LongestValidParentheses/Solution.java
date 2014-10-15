4c55c4452b19dd301f5e0c160c04248b3df8be2a
Leo Jiang <leo.jiang.dev@gmail.com>
2014-10-15 22:09:00 +0800

    add the comment and optimize.

--- src/main/java/com/leohacker/leetcode/LongestValidParentheses/Solution.java
+++ src/main/java/com/leohacker/leetcode/LongestValidParentheses/Solution.java
@@ -3,7 +3,7 @@ package com.leohacker.leetcode.LongestValidParentheses;
 /**
  * 基本的思路是每次遇到右括号，就开始处理前面未处理的左括号。
  *
- * 考虑()这种情况，右括号的出现就吃掉一个左括号，当前长度是2。如果是(())这种情况，在出现第一个右括号的时候，待处理的数据是(2。
+ * 考虑()这种情况，右括号的出现就吃掉一个左括号，当前长度是2。如果是(())这种情况，在出现第一个右括号的时候，处理的结果是(2。
  * 这时处理下一个右括号的时候，吃掉左括号，将当前长度加2。这样，一边扫描字符串的过程，就是将左括号和中间结果压栈，出现右括号弹栈的过程。
  * 我们就可以用一个栈来存储未处理的数据。
  *
@@ -13,6 +13,8 @@ package com.leohacker.leetcode.LongestValidParentheses;
  *
  * 由于可能到最后都没有出现可匹配的右括号，所以最后栈中可能还有未处理的数据。
  * 所以在最后要扫描一边栈，找出可能的最大长度。不要忘记还有不在栈里面的当前长度。
+ *
+ * 如果栈中数据被吃完，就计算出一段字符串的结果，就可以计算局部的最大长度。
  */
 public class Solution {
     public int longestValidParentheses(String s) {
@@ -20,6 +22,11 @@ public class Solution {
             return 0;
         }
         int len = s.length();
+
+        // give the result ASAP.
+        if (len == 0 || len == 1) {
+            return 0;
+        }
 
         // Use the stack to store the incomplete data. The max size for stack is the length of string.
         int[] stack = new int[len];
@@ -40,14 +47,15 @@ public class Solution {
                 index++;
                 // reset the current length since the left parentheses appears and interrupt the continuation.
                 currentLength = 0;
-            } else if ( index == 0 ) {  // right parentheses.
+            } else if ( index == 0 ) {
                 // index == 0 means that we have eaten all left parentheses, orphan right parentheses.
                 // calculate the max length and current length.
                 maxLength = ( maxLength > currentLength ) ? maxLength : currentLength ;
-                // since the orphan parentheses interrupt the continuation, we reset the current length.
+                // reset current length since the orphan parentheses interrupt the continuation.
                 currentLength = 0;
-            } else {    // index point to the next empty slot.
-                // eat the current left parentheses.
+            } else {
+                // eat the current left parentheses
+                // decrease the index first since the index point to the next empty slot after last element.
                 index--;                // popup the last left parentheses.
                 currentLength += 2;     // add the current length.
                 // merge the previous length number.

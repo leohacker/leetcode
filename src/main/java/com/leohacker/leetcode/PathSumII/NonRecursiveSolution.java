7015663e0d722d639631023854e13d6303fe1583
Leo Jiang <leo.jiang.dev@gmail.com>
2014-10-17 11:43:14 +0800

    Path Sum II solutions.

---
+++ src/main/java/com/leohacker/leetcode/PathSumII/NonRecursiveSolution.java
@@ -0,0 +1,61 @@
+package com.leohacker.leetcode.PathSumII;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Stack;
+
+/**
+ * Created by leojiang on 10/16/14.
+ */
+public class NonRecursiveSolution {
+    public List<List<Integer>> pathSum(TreeNode root, int sum) {
+        List<List<Integer>> result = new ArrayList<List<Integer>>();
+        if ( root == null ) {
+            return result;
+        }
+
+        Stack<TreeNode> stack = new Stack<TreeNode>();
+        Stack<Boolean> flag = new Stack<Boolean>();
+        stack.push(root);
+        flag.push(false);
+
+        // current equals the sum of val in the stack.
+        int current = root.val;
+
+        while ( !stack.isEmpty() ) {
+            TreeNode node = stack.pop();
+            boolean isAccessed = flag.pop();
+
+            if ( node.left == null && node.right == null ) {
+                if ( !isAccessed && current == sum ) {
+                    List<Integer> list = new ArrayList<Integer>();
+                    for ( TreeNode n : stack ) {
+                        list.add(new Integer(n.val));
+                    }
+                    list.add(new Integer(node.val));
+                    result.add(list);
+                }
+                current -= node.val;
+            }
+
+            if ( node.left != null ) {
+                TreeNode left = node.left;
+                node.left = null;
+                stack.push(node);
+                flag.push(true);
+                stack.push(left);
+                flag.push(false);
+                current += left.val;
+            } else if ( node.right != null ) {
+                TreeNode right = node.right;
+                node.right = null;
+                stack.push(node);
+                flag.push(true);
+                stack.push(right);
+                flag.push(false);
+                current += right.val;
+            }
+        }
+        return result;
+    }
+}
---
+++ src/main/java/com/leohacker/leetcode/PathSumII/TreeNode.java
@@ -0,0 +1,8 @@
+package com.leohacker.leetcode.PathSumII;
+
+public class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+    TreeNode(int x) { val = x; }
+}

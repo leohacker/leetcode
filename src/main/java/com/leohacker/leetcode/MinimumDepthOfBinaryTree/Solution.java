053f7505e5c0c2f13f07964c4f94e88a634d38c4
Leo Jiang <leo.jiang.dev@gmail.com>
2014-10-20 13:29:23 +0800

    Minimum Depth of Binary Tree.

--- src/main/java/com/leohacker/leetcode/MinimumDepthOfBinaryTree/Solution.java
+++ src/main/java/com/leohacker/leetcode/MinimumDepthOfBinaryTree/Solution.java
@@ -6,17 +6,38 @@ import java.util.ArrayList;
 import java.util.List;
 
 /**
- * Created by leojiang on 10/20/14.
+ * Still can be optimized.
  */
 public class Solution {
     public int minDepth(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
         List<TreeNode> list = new ArrayList<TreeNode>();
         list.add(root);
 
-        while (!list.isEmpty()) {
-
-
+        TreeNode p = null;
+        int level = 0;
+        boolean found = false;
+        while (!list.isEmpty() && !found) {
+            level++;
+            int len = list.size();
+            for (int i = 0; i < len; i++) {
+                p = list.get(i);
+                if (p.left != null) {
+                    list.add(p.left);
+                }
+                if (p.right != null) {
+                    list.add(p.right);
+                }
+                if (p.left == null && p.right == null) {
+                    found = true;
+                }
+            }
+            for (int i = 0; i < len; i++) {
+                list.remove(0);
+            }
         }
-        return 0;
+        return level;
     }
 }

a99fedaa5c819c991ce03d14e25000a8e9d9344f
Leo Jiang <leo.jiang.dev@gmail.com>
2014-10-20 16:48:01 +0800

    remove element.

---
+++ src/main/java/com/leohacker/leetcode/RemoveElement/Solution.java
@@ -0,0 +1,25 @@
+package com.leohacker.leetcode.RemoveElement;
+
+/**
+ * Created by leojiang on 10/20/14.
+ */
+public class Solution {
+    public int removeElement(int[] A, int elem) {
+        if (A.length == 0) {
+            return 0;
+        }
+        int left = 0;
+        int right = A.length - 1;
+
+        while ( left != right ) {
+            if (A[left] != elem) {
+                left++;
+            } else {
+                A[left] = A[right];
+                right--;
+            }
+        }
+
+        return A[left] == elem ? left : left + 1;
+    }
+}

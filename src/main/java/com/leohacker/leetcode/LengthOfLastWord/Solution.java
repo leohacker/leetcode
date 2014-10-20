8fc3f17edcf4a14485a41529397ff1011f0179d7
Leo Jiang <leo.jiang.dev@gmail.com>
2014-10-20 17:02:37 +0800

    length of last word.

---
+++ src/main/java/com/leohacker/leetcode/LengthOfLastWord/Solution.java
@@ -0,0 +1,15 @@
+package com.leohacker.leetcode.LengthOfLastWord;
+
+/**
+ * Created by leojiang on 10/20/14.
+ */
+public class Solution {
+    public int lengthOfLastWord(String s) {
+        String[] words = s.split("\\s");
+        if ( words.length == 0) {
+            return 0;
+        }
+        String last = words[words.length -1];
+        return last.length();
+    }
+}

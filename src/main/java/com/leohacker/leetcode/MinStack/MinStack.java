910129f4ce4f26e0719fb039dc4422a7f1dbbfce
Leo Jiang <leo.jiang.dev@gmail.com>
2014-11-27 21:09:36 +0800

    MinStack.

---
+++ src/main/java/com/leohacker/leetcode/MinStack/MinStack.java
@@ -0,0 +1,33 @@
+package com.leohacker.leetcode.MinStack;
+
+import java.util.Stack;
+
+/**
+ * Created by leojiang on 11/27/14.
+ */
+class MinStack {
+    public void push(int x) {
+        regularStack.push(x);
+        if ( minStack.isEmpty() || x < minStack.peek() ) {
+            minStack.push(x);
+        } else {
+            minStack.push(minStack.peek());
+        }
+    }
+
+    public void pop() {
+        regularStack.pop();
+        minStack.pop();
+    }
+
+    public int top() {
+        return regularStack.peek();
+    }
+
+    public int getMin() {
+        return minStack.peek();
+    }
+
+    private Stack<Integer> regularStack = new Stack<Integer>();
+    private Stack<Integer> minStack = new Stack<Integer>();
+}
---
+++ src/main/java/com/leohacker/leetcode/MinStack/OptimizedMinStack.java
@@ -0,0 +1,28 @@
+package com.leohacker.leetcode.MinStack;
+
+import java.util.Stack;
+
+// Please refer: https://oj.leetcode.com/problems/min-stack/solution/
+class OptimizedMinStack {
+    private Stack<Integer> stack = new Stack<>();
+    private Stack<Integer> minStack = new Stack<>();
+
+    public void push(int x) {
+        stack.push(x);
+        if (minStack.isEmpty() || x <= minStack.peek()) {
+            minStack.push(x);
+        }
+    }
+
+    public void pop() {
+        if (stack.pop().equals(minStack.peek())) minStack.pop();
+    }
+
+    public int top() {
+        return stack.peek();
+    }
+
+    public int getMin() {
+        return minStack.peek();
+    }
+}
